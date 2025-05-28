098007de40464b7942aafa9bb56e10735d4e3348
Andy Guo <andysaikou2526@gmail.com>
2025-05-28 12:50:52 +0100

    feat(ui): implement basic homepage ui

---
+++ lib/home_page.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:intl/intl.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _formattedTime = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _updateTime();
+    // Update time every minute
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      _updateTime();
+    });
+  }
+
+  void _updateTime() {
+    final now = DateTime.now();
+    final formatter = DateFormat('HH:mm');
+    setState(() {
+      _formattedTime = formatter.format(now);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Image.asset(
+              'assets/day-left.png',
+              fit: BoxFit.fitHeight,
+              height: MediaQuery.of(context).size.height,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(
+              0,
+              -screenHeight * 0.27,
+            ), // Adjust the vertical position
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    _formattedTime,
+                    style: TextStyle(
+                      fontSize: 90,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white.withValues(alpha: 10),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Handle sleep action
+                      ScaffoldMessenger.of(
+                        context,
+                      ).showSnackBar(SnackBar(content: Text('Good night!')));
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white.withValues(alpha: 10),
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 32,
+                        vertical: 16,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    child: Text('Go to sleep', style: TextStyle(fontSize: 18)),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
