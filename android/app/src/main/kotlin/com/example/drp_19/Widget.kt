6f890bf172490cffee9b37ecca6bfa790b0043ab
Charbs <charbs@dyn26157-87.wlan.ic.ac.uk>
2025-06-03 14:14:57 +0100

    feat: scaffold android widget

---
+++ android/app/src/main/kotlin/com/example/drp_19/Widget.kt
@@ -0,0 +1,42 @@
+package com.example.drp_19
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+import android.app.PendingIntent
+import android.content.Intent
+
+class Widget : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // Perform this loop procedure for each widget that belongs to this
+        // provider.
+        appWidgetIds.forEach { appWidgetId ->
+            // Create an Intent to launch ExampleActivity.
+            val pendingIntent: PendingIntent = PendingIntent.getActivity(
+                /* context = */ context,
+                /* requestCode = */  0,
+                /* intent = */ Intent(context, MainActivity::class.java),
+                /* flags = */ PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+
+            // Get the layout for the widget and attach an onClick listener to
+            // the button.
+            val views: RemoteViews = RemoteViews(
+                context.packageName,
+                R.layout.widget
+            ).apply {
+                setOnClickPendingIntent(R.id.widget_container, pendingIntent)
+            }
+
+            // Tell the AppWidgetManager to perform an update on the current
+            // widget.
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
