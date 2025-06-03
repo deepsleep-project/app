50affca00fb6fd6da225df64189b9198286b75d9
Ningchuan Wang <nw523@ic.ac.uk>
2025-06-03 21:47:45 +0100

    Feat: add a notification on setting(nw523)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,29 @@
 import 'package:drp_19/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() {
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+
+  final DarwinInitializationSettings initializationSettingsDarwin =
+      DarwinInitializationSettings();
+
+  final InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+    iOS: initializationSettingsDarwin,
+  );
+
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  if (await Permission.notification.isDenied) {
+    await Permission.notification.request();
+  }
   runApp(const MyApp());
 }
 
